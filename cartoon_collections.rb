e10088bb74fe152ddfaaf3450e8db232802b6062
Clarion Heard <clarion.heard@gmail.com>
2020-01-05 15:17:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(ingredients)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.
+  ingredients.find{|word|
+    cheese_types.include?(word)}
 end
